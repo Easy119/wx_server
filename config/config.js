7121a8ca6bd07ae50bdd1892860a38c088b50c71
Easy119 <33249193+Easy119@users.noreply.github.com>
2019-05-15 11:41:20 +0800

    Update config.js

--- config/config.js
+++ config/config.js
@@ -4,8 +4,8 @@ const util = require("../libs/util")
 module.exports = {
     wechat: {
         token: "20180416",
-        appID: "wxebd0b2e5bcc4d555",
-        appsecret: "d38a3f86111c5627da616502883bf296",
+        appID: "wxebd0b2e5bcd4d555",
+        appsecret: "d38a3f86111c5627dc616502883bf296",
         getAccessToken(){
             return util.readFileAsync(wechat_file)
         },
